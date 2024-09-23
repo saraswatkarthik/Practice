4efaf0ea1b6a89bb0be7a47f2f8cadd9312b1ffe
saraswatkarthik <84384818+saraswatkarthik@users.noreply.github.com>
2024-09-23 17:39:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 var kiloMeters = document.getelementbyid("kms").value;
 var nmDiv = document.getelementbyid("nm");
 var nauticalMiles = kiloMeters * 0.53;
-nmDiv.innerHTML = nauticalMiles;
+nmDiv.innerText = nauticalMiles;
